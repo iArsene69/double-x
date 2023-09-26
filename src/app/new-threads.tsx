18191a0ea7ef99a4e96334db6fcddbe1f8ed1f5a
iArsene69 <mirzaprati@gmail.com>
2023-09-26 19:13:30 +0700

    ready

--- src/app/new-threads.tsx
+++ src/app/new-threads.tsx
@@ -1,28 +1,83 @@
-import { User, createServerActionClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
-import Image from "next/image";
+"use client";
 
-export const dynamic = "force-dynamic";
+import AddImage from "@/components/add-image";
+import ImagePlaceHolder from "@/components/place-image";
+import {
+  User,
+  createClientComponentClient,
+} from "@supabase/auth-helpers-nextjs";
+import Image from "next/image";
+import { useState } from "react";
+import { AiOutlineSend } from "react-icons/ai";
+import { v4 as uuidv4 } from "uuid";
 
 export default function NewThreads({ user }: { user: User }) {
-  const addThreads = async (formData: FormData) => {
-    "use server";
-    const thread = String(formData.get("thread"));
-    const supabase = createServerActionClient<Database>({ cookies });
-    
-    await supabase.from("threads").insert({ thread, user_id: user?.id });
+  const [thread, setThread] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [imageFile, setImageFile] = useState<File>();
+
+  const supabase = createClientComponentClient();
+
+  const uploadImage = async (uuid: string) => {
+    if (!imageFile) return;
+    const name = `public_images_${uuid}`;
+    await supabase.storage.from("images").upload(name, imageFile);
+
+    const {
+      data: { publicUrl },
+    } = await supabase.storage.from("images").getPublicUrl(`/${name}`);
+
+    return publicUrl;
   };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const publicUrl = await uploadImage(uuidv4());
+    await supabase
+      .from("threads")
+      .insert({ thread, image_url: publicUrl, user_id: user?.id });
+    setThread("");
+    setImageUrl("");
+    setImageFile(undefined);
+  };
+
+  const deleteImage = () => {
+    setImageUrl("");
+    setImageFile(undefined);
+  };
+
   return (
-    <form action={addThreads} className="border border-gray-800 border-t-0">
-      <div className="flex px-4 py-6 items-center">
-        <Image src={user.user_metadata.avatar_url} alt="user profile" width={48} height={48} className="rounded-full" />
+    <form onSubmit={handleSubmit} className="border border-gray-800 border-t-0">
+      <div className="flex px-4 pt-6 items-center">
+        <Image
+          src={user.user_metadata.avatar_url}
+          alt="user profile"
+          width={48}
+          height={48}
+          className="rounded-full"
+        />
         <input
+          onChange={(e) => {
+            setThread(e.target.value);
+          }}
+          value={thread}
+          required
           placeholder="Whatsup'"
           type="text"
           name="thread"
-          className="bg-inherit ml-4 px-2 leading-loose text-xl placeholder-gray-500 rounded-md py-2 flex-1 outline-none"
+          className="bg-inherit ml-4 px-2 leading-loose text-xl placeholder-gray-500 rounded-md py-2 flex-1 outline-none resize-y"
         />
-        <button type="submit" className="bg-blue-500 rounded-xl py-2 px-6 text-center font-semibold">Send</button>
+      </div>
+      <div className="w-fit mx-auto">
+        <ImagePlaceHolder src={imageUrl} close={deleteImage} />
+      </div>
+      <div className="flex justify-around gap-x-52 items-center mx-5 my-3">
+        <div className="flex justify-center">
+          <AddImage setImageUrl={setImageUrl} setImageFile={setImageFile} />
+        </div>
+        <button type="submit">
+          <AiOutlineSend className="text-blue-500 w-8 h-8" />
+        </button>
       </div>
     </form>
   );
--- src/app/reply/new-replies.tsx
+++ src/app/reply/new-replies.tsx
@@ -1,25 +1,86 @@
-import { createServerActionClient,  User  } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
+"use client";
+
+import AddImage from "@/components/add-image";
+import ImagePlaceHolder from "@/components/place-image";
+import {
+  createClientComponentClient,
+  User,
+} from "@supabase/auth-helpers-nextjs";
 import Image from "next/image";
+import { useState } from "react";
+import { AiOutlineSend } from "react-icons/ai";
+import { v4 as uuidv4 } from "uuid";
 
-export const dynamic = "force-dynamic"
+export default function NewReplies({
+  user,
+  threadId,
+}: {
+  user: User | undefined;
+  threadId: string;
+}) {
+  const [reply, setReply] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [imageFile, setImageFile] = useState<File>();
 
-export default function NewReplies({user, threadId}: {user: User | undefined; threadId: string}){
-    const addReply = async (formData: FormData) => {
-        "use server"
-        const reply = String(formData.get("reply"));
-        const supabase = createServerActionClient({cookies})
+  const supabase = createClientComponentClient();
 
-        await supabase.from("replies").insert({reply, user_id: user?.id, thread_id: threadId})
-    }
+  const uploadImage = async (uuid: string) => {
+    if (!imageFile) return;
+    const name = `public_reply_${uuid}`;
+    await supabase.storage.from("images").upload(name, imageFile);
 
-    return(
-        <form action={addReply} className="border border-gray-800 border-t-0">
-            <div className="flex px-4 py-6 items-center">
-                <Image src={user?.user_metadata.avatar_url} alt="user profile" width={48} height={48} className="rounded-full" />
-                <input type="text" placeholder="Wha'dya think?" name="reply" className="bg-inherit ml-4 px-2 leading-loose text-xl placeholder-gray-500 rounded-md py-2 flex-1 outline-none" />
-                <button type="submit" className="bg-blue-500 rounded-xl py-2 px-6 text-center font-semibols">Send</button>
-            </div>
-        </form>
-    )
+    const {
+      data: { publicUrl },
+    } = await supabase.storage.from("images").getPublicUrl(`/${name}`);
+
+    return publicUrl;
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const publicUrl = await uploadImage(uuidv4());
+    await supabase
+      .from("replies")
+      .insert({ reply, user_id: user?.id, thread_id: threadId, image_url: publicUrl });
+    setReply("");
+    setImageUrl("");
+    setImageFile(undefined);
+  };
+
+  const deleteImage = () => {
+    setImageUrl("");
+    setImageFile(undefined);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="border border-gray-800 border-t-0">
+      <div className="flex px-4 pt-6 items-center">
+        <Image
+          src={user?.user_metadata.avatar_url}
+          alt="user profile"
+          width={48}
+          height={48}
+          className="rounded-full"
+        />
+        <input
+          onChange={(e) => setReply(e.target.value)}
+          type="text"
+          placeholder="Wha'dya think?"
+          value={reply}
+          className="bg-inherit ml-4 px-2 leading-loose text-xl placeholder-gray-500 rounded-md py-2 flex-1 outline-none"
+        />
+      </div>
+      <div className="w-fit mx-auto">
+        <ImagePlaceHolder src={imageUrl} close={deleteImage} />
+      </div>
+      <div className="flex justify-around gap-x-52 items-center mx-5 my-3">
+        <div className="flex justify-center">
+          <AddImage setImageUrl={setImageUrl} setImageFile={setImageFile} />
+        </div>
+        <button type="submit">
+          <AiOutlineSend className="text-blue-500 w-8 h-8" />
+        </button>
+      </div>
+    </form>
+  );
 }
